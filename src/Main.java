2433ec4d21fe00f43c691ab38bf31759b54b1fc4
Alexandros Chanas <alexnwris@Live.com>
2026-02-02 16:19:49 +0200

    Completition of the primeNumbers

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,35 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int start = 1;
+        int end = 6;
+        int primeNumbersFound = 0;
 
-        for(int i = 1; i<=100; i++){
+        for(int i = start; i<=end; i++){
 
                 if(isPrime(i)){
+
                 System.out.println("Is " + i + " a prime? => " + isPrime(i));
+
+                primeNumbersFound++;
+
+                if(primeNumbersFound == 3){
+                    break;
+                }
+
                 }
 
         }
 
+    System.out.println("Total prime numbers found between " + start + " to " +  end  + " are => " + primeNumbersFound );
+
     }
 
+
+
+
     public static boolean isPrime(int prime){
+    int primeNumbersFound = 0;
 
     if(prime < 2){
         return false;
@@ -27,6 +44,8 @@ public class Main {
 
 
     }
+
+    primeNumbersFound++;
     return true;
 
     }
